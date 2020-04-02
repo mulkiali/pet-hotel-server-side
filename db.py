6feb0d294e59f6fef06ed05588fca4418e061c0a
Jessie Conley <jconley2881@gmail.com>
2020-04-02 11:50:58 -0500

    db printing in terminal from db

--- db.py
+++ db.py
@@ -2,23 +2,21 @@ import psycopg2
 
 ##connect to the db
 con = psycopg2.connect(
-    host = 'localmachinename here',
-    database="databasenamehere",
-    user = 'postgres',
-    password="postgres",
-    port = 5432
+    host = 'localhost',
+    database="pet_hotel"
+   
 )
 
 ##curser = commands used to communicate with DB
 cur = con.cursor()
 ##execute query
-cur.execute("select id, name from "database name")
+cur.execute("select * from owners")
 
 rows = cur.fetchall()
 
 for r in rows:
     print (f"id {r[0]} name {r[1]}")
-    
+
 ##close cursor
     cur.close()
 ##close the connection
